4e5a5f6e9b9a8357358b1729683dde02ce7fd4be
Vineeth Aitha <vineethaitha1234@gmail.com>
2025-11-03 11:13:55 +0530

    MVC Implementation

---
+++ TinyVitals/Controllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TinyVitals
+//
+//  Created by user70 on 01/11/25.
+//
+
+import UIKit
+import QuartzCore
+
+class ViewController: UIViewController {
+
+    @IBOutlet var gradientView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
---
+++ TinyVitals/Controllers/loginScreenViewController.swift
@@ -0,0 +1,34 @@
+//
+//  loginScreenViewController.swift
+//  TinyVitals
+//
+//  Created by user70 on 03/11/25.
+//
+
+import UIKit
+
+class loginScreenViewController: UIViewController {
+
+    
+    @IBOutlet var tinyVitalsTitle: UILabel!
+    
+    @IBOutlet var tinyVitalsCaption: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tinyVitalsTitle.font=UIFont(name: "Sigmar-Regular", size: 40)
+        tinyVitalsCaption.font=UIFont(name: "Sigmar-Regular", size: 24)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
