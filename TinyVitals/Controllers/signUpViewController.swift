e55cfc6742bd8c067ba9e8e1f90bb67743a9db7b
Vineeth Aitha <vineethaitha1234@gmail.com>
2025-11-04 21:40:05 +0530

    Worked in homescreen UI

---
+++ TinyVitals/Controllers/signUpViewController.swift
@@ -0,0 +1,25 @@
+//
+//  signUpViewController.swift
+//  TinyVitals
+//
+//  Created by user70 on 03/11/25.
+//
+
+import UIKit
+
+class signUpViewController: UIViewController {
+
+    
+    @IBOutlet var tinyVitalsTitle: UILabel!
+    
+    @IBOutlet var tinyVitalsCaption: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tinyVitalsTitle.font = UIFont(name: "Sigmar-Regular", size: 40)
+        tinyVitalsCaption.font = UIFont(name: "Sigmar-Regular", size: 24
+        )
+        
+    }
+
+}
