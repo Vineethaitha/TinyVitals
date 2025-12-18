91337121e376826119e13138e4ad4f3ce4f424b9
Vineeth Aitha <vineethaitha1234@gmail.com>
2025-12-18 11:51:41 +0530

    Initial Commit

---
+++ TinyVitals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinyVitals
+//
+//  Created by admin0 on 12/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TinyVitalsTests/TinyVitalsTests.swift
@@ -0,0 +1,17 @@
+//
+//  TinyVitalsTests.swift
+//  TinyVitalsTests
+//
+//  Created by admin0 on 12/18/25.
+//
+
+import Testing
+@testable import TinyVitals
+
+struct TinyVitalsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
