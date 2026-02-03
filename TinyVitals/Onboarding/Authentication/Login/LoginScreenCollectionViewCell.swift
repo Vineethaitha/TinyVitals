6a55d7bec4363d78fb24048de2e6c01c98751ec5
Vineeth Aitha <vineethaitha1234@gmail.com>
2026-02-03 09:49:23 +0530

    made some ui changes in onboardingVC

---
+++ TinyVitals/Onboarding/Authentication/Login/LoginScreenCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  TinyVitals
+//
+//  Created by admin0 on 2/3/26.
+//
+
+import UIKit
+
+class LoginScreenCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ TinyVitals/Onboarding/Authentication/Login/OnboardingSlide.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingSlide.swift
+//  TinyVitals
+//
+//  Created by admin0 on 2/3/26.
+//
+
+import Foundation
