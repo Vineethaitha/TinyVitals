11bf30bfe5f87813d264a75776e55be0ad282beb
Vineeth Aitha <vineethaitha1234@gmail.com>
2026-03-30 21:24:44 +0530

    fixed bugs in article fetching......

--- TinyVitals/HomeScreen/Controllers/ArticleCardCell.swift
+++ TinyVitals/HomeScreen/Controllers/ArticleCardCell.swift
@@ -17,6 +17,7 @@ final class ArticleCardCell: UICollectionViewCell {
     private var lottieAnimView: LottieAnimationView?
     private var localImageView: UIImageView?
     private var loadingIndicator: UIActivityIndicatorView?
+    private var currentMediaURL: String?
 
     override func prepareForReuse() {
         super.prepareForReuse()
@@ -39,6 +40,7 @@ final class ArticleCardCell: UICollectionViewCell {
     func configure(title: String, subtitle: String, mediaType: String, mediaURL: String) {
         titleLabel.text = title
         subtitleLabel.text = subtitle
+        currentMediaURL = mediaURL
         
         clearMedia()
         
@@ -55,61 +57,100 @@ final class ArticleCardCell: UICollectionViewCell {
         self.loadingIndicator = spinner
         
         if mediaType.lowercased() == "json" {
-            guard let url = URL(string: mediaURL) else { return }
-            LottieAnimation.loadedFrom(url: url) { [weak self] animation in
-                self?.loadingIndicator?.stopAnimating()
-                self?.loadingIndicator?.removeFromSuperview()
-                self?.loadingIndicator = nil
+            if mediaURL.lowercased().hasPrefix("http") {
+                let encodedString = mediaURL.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? mediaURL
+                let finalUrlStr = URL(string: mediaURL) != nil ? mediaURL : encodedString
+                guard let url = URL(string: finalUrlStr) else { return }
                 
-                guard let self = self, let animation = animation else { return }
+                LottieAnimation.loadedFrom(url: url) { [weak self] animation in
+                    guard let self = self, self.currentMediaURL == mediaURL else { return }
+                    
+                    self.loadingIndicator?.stopAnimating()
+                    self.loadingIndicator?.removeFromSuperview()
+                    self.loadingIndicator = nil
+                    
+                    guard let animation = animation else { return }
+                    self.displayLottie(animation)
+                }
+            } else {
+                // Local Lottie Fallback
+                self.loadingIndicator?.stopAnimating()
+                self.loadingIndicator?.removeFromSuperview()
+                self.loadingIndicator = nil
                 
-                let animView = LottieAnimationView(animation: animation)
-                animView.loopMode = .loop
-                animView.contentMode = .scaleAspectFit
-                animView.translatesAutoresizingMaskIntoConstraints = false
-                animView.animationSpeed = 0.9
-
-                self.animationContainerView.addSubview(animView)
-
-                NSLayoutConstraint.activate([
-                    animView.topAnchor.constraint(equalTo: self.animationContainerView.topAnchor),
-                    animView.bottomAnchor.constraint(equalTo: self.animationContainerView.bottomAnchor),
-                    animView.leadingAnchor.constraint(equalTo: self.animationContainerView.leadingAnchor),
-                    animView.trailingAnchor.constraint(equalTo: self.animationContainerView.trailingAnchor)
-                ])
-
-                animView.play()
-                self.lottieAnimView = animView
+                if let animation = LottieAnimation.named(mediaURL) {
+                    self.displayLottie(animation)
+                }
             }
         } else {
-            guard let url = URL(string: mediaURL) else { return }
-            let imgView = UIImageView()
-            imgView.contentMode = .scaleAspectFill
-            imgView.clipsToBounds = true
-            imgView.translatesAutoresizingMaskIntoConstraints = false
-            
-            animationContainerView.addSubview(imgView)
-            
-            NSLayoutConstraint.activate([
-                imgView.topAnchor.constraint(equalTo: animationContainerView.topAnchor),
-                imgView.bottomAnchor.constraint(equalTo: animationContainerView.bottomAnchor),
-                imgView.leadingAnchor.constraint(equalTo: animationContainerView.leadingAnchor),
-                imgView.trailingAnchor.constraint(equalTo: animationContainerView.trailingAnchor)
-            ])
-            
-            self.localImageView = imgView
-            
-            URLSession.shared.dataTask(with: url) { [weak imgView, weak self] data, _, _ in
-                DispatchQueue.main.async {
-                    self?.loadingIndicator?.stopAnimating()
-                    self?.loadingIndicator?.removeFromSuperview()
-                    self?.loadingIndicator = nil
-                    
-                    if let data = data, let image = UIImage(data: data) {
-                        imgView?.image = image
+            if mediaURL.lowercased().hasPrefix("http") {
+                let encodedString = mediaURL.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? mediaURL
+                let finalUrlStr = URL(string: mediaURL) != nil ? mediaURL : encodedString
+                guard let url = URL(string: finalUrlStr) else { return }
+                
+                let imgView = self.createImageView()
+                self.localImageView = imgView
+                
+                URLSession.shared.dataTask(with: url) { [weak imgView, weak self] data, _, _ in
+                    DispatchQueue.main.async {
+                        guard let self = self, self.currentMediaURL == mediaURL else { return }
+                        
+                        self.loadingIndicator?.stopAnimating()
+                        self.loadingIndicator?.removeFromSuperview()
+                        self.loadingIndicator = nil
+                        
+                        if let data = data, let image = UIImage(data: data) {
+                            imgView?.image = image
+                        }
                     }
-                }
-            }.resume()
+                }.resume()
+            } else {
+                // Local Image Fallback
+                self.loadingIndicator?.stopAnimating()
+                self.loadingIndicator?.removeFromSuperview()
+                self.loadingIndicator = nil
+                
+                let imgView = self.createImageView()
+                self.localImageView = imgView
+                imgView.image = UIImage(named: mediaURL)
+            }
         }
+    }
+    
+    private func displayLottie(_ animation: LottieAnimation) {
+        let animView = LottieAnimationView(animation: animation)
+        animView.loopMode = .loop
+        animView.contentMode = .scaleAspectFit
+        animView.translatesAutoresizingMaskIntoConstraints = false
+        animView.animationSpeed = 0.9
+
+        self.animationContainerView.addSubview(animView)
+
+        NSLayoutConstraint.activate([
+            animView.topAnchor.constraint(equalTo: self.animationContainerView.topAnchor),
+            animView.bottomAnchor.constraint(equalTo: self.animationContainerView.bottomAnchor),
+            animView.leadingAnchor.constraint(equalTo: self.animationContainerView.leadingAnchor),
+            animView.trailingAnchor.constraint(equalTo: self.animationContainerView.trailingAnchor)
+        ])
+
+        animView.play()
+        self.lottieAnimView = animView
+    }
+
+    private func createImageView() -> UIImageView {
+        let imgView = UIImageView()
+        imgView.contentMode = .scaleAspectFill
+        imgView.clipsToBounds = true
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.animationContainerView.addSubview(imgView)
+        
+        NSLayoutConstraint.activate([
+            imgView.topAnchor.constraint(equalTo: self.animationContainerView.topAnchor),
+            imgView.bottomAnchor.constraint(equalTo: self.animationContainerView.bottomAnchor),
+            imgView.leadingAnchor.constraint(equalTo: self.animationContainerView.leadingAnchor),
+            imgView.trailingAnchor.constraint(equalTo: self.animationContainerView.trailingAnchor)
+        ])
+        return imgView
     }
 }
